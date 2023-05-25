021bb0d46477ee8a2853763952198a8edd3757c5
Pedro Chamone <99288069+Chamonex@users.noreply.github.com>
2023-05-25 10:59:18 -0300

    Add files via upload
    1.1

--- main.py
+++ main.py
@@ -1,22 +1,48 @@
 import string
 import random
 
-senha =""
-
 caracteres = {
-    0: [string.ascii_letters, len(string.ascii_letters)],
-    1: [string.digits, len(string.digits)],
+    0: [string.digits, len(string.digits)],
+    1: [string.ascii_letters, len(string.ascii_letters)],
     2: [string.punctuation, len(string.punctuation)]
 }
 
-def generate_password(senha):
+def generate_password(option):
 
-    for i in range(10):
+    password =""
 
-        x = random.randint(0, 2)
-        y = random.randint(0, caracteres[x][1])
-        novo = caracteres[x][0][random.randint(0, y)]
-        senha += novo
+    if option == 1:                     #somente numeros
+        for i in range(10):
+            m = caracteres[0][1]
+            new = caracteres[0][0][random.randint(0, m)]
+            password += new
 
-print(senha)
+        return password
+
+    elif option == 2:                   #numeros e letras
+        for i in range(10):
+            x = random.randint(0, 1)
+            m = caracteres[x][1]
+            new = caracteres[x][0][random.randint(0, m)]
+            password += new
+
+        return password
+
+    else:
+        for i in range(10):             #nueros, letras e pontuação
+
+            x = random.randint(0, 2)
+            m = caracteres[x][1]
+            new = caracteres[x][0][random.randint(0, m)]
+            password += new
+
+        return password
+
+
+print("1 -> somente numeros\n2 -> numeros e letras\n3 -> nomeros letras e pontuacao\n")
+
+
+option = input()
+
+print(generate_password(option))
 
